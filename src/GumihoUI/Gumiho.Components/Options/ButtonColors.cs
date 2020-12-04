61bc90927883787ef5a20a09ec02907f6efd0615
Adam Jaguś <13621655+ItsMeSousuke@users.noreply.github.com>
2020-12-05 00:16:35 +0100

    extend GButton component

---
+++ src/GumihoUI/Gumiho.Components/Options/ButtonColors.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GumihoUI
+{
+    public static class ButtonColors
+    {
+        public static readonly string Blue = "blue";
+        public static readonly string Red = "red";
+        public static readonly string Orange = "orange";
+        public static readonly string Green = "green";
+        public static readonly string White = "white";
+        public static readonly string Gray = "gray";
+    }
+}
