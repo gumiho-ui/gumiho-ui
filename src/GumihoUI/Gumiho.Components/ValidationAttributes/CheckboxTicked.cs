651df54fddd6d5a7aeac6f2386cd05475baa9f9b
Sousuke <13621655+ItsMeSousuke@users.noreply.github.com>
2021-02-02 21:30:02 +0100

    Checkbox component (#10)
    * add checkbox component with styling

---
+++ src/GumihoUI/Gumiho.Components/ValidationAttributes/CheckboxTicked.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GumihoUI
+{
+    public class CheckboxTicked : ValidationAttribute
+    {
+        public override bool IsValid(object? value) =>
+            value is not null && value is bool boolean && boolean;
+    }
+}
