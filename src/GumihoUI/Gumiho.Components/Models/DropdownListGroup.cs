f510dbb5ab164df11fd636d51bab02c7c55e23de
Adam Jaguś <13621655+ItsMeSousuke@users.noreply.github.com>
2021-03-15 22:32:04 +0100

    add dropdown models

---
+++ src/GumihoUI/Gumiho.Components/Models/DropdownListGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GumihoUI.Models
+{
+    public class DropdownListGroup
+    {
+        /// <summary>
+        /// Gets or sets <see cref="DropdownListGroup" /> Name.
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Gets or sets a value that indicate whether this <see cref="DropdownListGroup" /> is disabled.
+        /// </summary>
+        public bool Disabled { get; set; }
+    }
+}
---
+++ src/GumihoUI/Gumiho.Components/Models/DropdownListItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GumihoUI.Models
+{
+    public class DropdownListItem<T>
+    {
+        /// <summary>
+        /// Gets or sets a value that indicate whether this <see cref="DropdownListItem{T}" /> is selected.
+        /// </summary>
+        public bool Selected { get; set; }
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        public T Value { get; set; }
+        /// <summary>
+        /// Gets or sets text.
+        /// </summary>
+        public string Text { get; set; }
+        /// <summary>
+        /// Gets or sets a value that indicate whether this <see cref="DropdownListItem{T}" /> is disabled.
+        /// </summary>
+        public bool Disabled { get; set; }
+        /// <summary>
+        /// Represents the group of dropdown list items.
+        /// </summary>
+        public DropdownListGroup Group { get; set; }
+    }
+}
