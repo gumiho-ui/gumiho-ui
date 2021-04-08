99cebdb7feba7dc25fdd8b30afa863897bd29f58
Adam Jaguś <13621655+ItsMeSousuke@users.noreply.github.com>
2021-04-08 23:48:37 +0200

    add validation. clear debug messages

---
+++ src/GumihoUI/ExamplesWASM/Validation/DropdownLowerThanFour.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExamplesWASM
+{
+    public class DropdownLowerThanFour : ValidationAttribute
+    {
+        public override bool IsValid(object? value) =>
+            value is not null && value is int val && val >= 4;
+    }
+}
