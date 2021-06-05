b357e11a27c11c650435a808e00db5139cfd527b
Adam Jaguś <13621655+ItsMeSousuke@users.noreply.github.com>
2021-06-05 15:26:56 +0200

    fix styling for focused items, fix error when select list is not hidding, add close select list on escape key

--- src/GumihoUI/ExamplesWASM/Validation/SelectLowerThanFour.cs
+++ src/GumihoUI/ExamplesWASM/Validation/SelectLowerThanFour.cs
@@ -2,7 +2,7 @@
 
 namespace ExamplesWASM
 {
-    public class DropdownLowerThanFour : ValidationAttribute
+    public class SelectLowerThanFour : ValidationAttribute
     {
         public override bool IsValid(object? value) =>
             value is not null && value is int val && val >= 4;
