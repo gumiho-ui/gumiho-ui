f005751da5b16b48d954753e7249be2f7da4a82f
Adam Jaguś <13621655+ItsMeSousuke@users.noreply.github.com>
2021-06-17 22:39:26 +0200

    add card and card content components

---
+++ src/GumihoUI/Gumiho.Components/Options/HorizontalAlign.cs
@@ -0,0 +1,10 @@
+﻿namespace GumihoUI
+{
+    public static class HorizontalAlign
+     {
+        public static readonly string Left = "text-align-left";
+        public static readonly string Center = "text-align-center";
+        public static readonly string Right = "text-align-right";
+        public static readonly string Justify = "text-align-justify";
+    }
+}
