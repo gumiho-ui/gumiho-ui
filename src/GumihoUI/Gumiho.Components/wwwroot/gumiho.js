d1781860c111920f03a3a4536c01ab17cfec933f
Adam Jaguś <13621655+ItsMeSousuke@users.noreply.github.com>
2021-07-02 23:42:26 +0200

    add navbar, navlogo, navgroup and navitem components with some of the logics and styles

---
+++ src/GumihoUI/Gumiho.Components/wwwroot/gumiho.js
@@ -0,0 +1,8 @@
+﻿window.scrollToElement = (elementId) => {
+    let element = document.getElementById(elementId)
+    console.log('aa', element)
+
+    if (element) {
+        element.scrollIntoView({ behavior: 'smooth' })
+    }
+}
