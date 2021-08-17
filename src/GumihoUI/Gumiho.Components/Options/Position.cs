5870045b0d0371cfedb673d227816dd23d093079
Adam Jaguś <13621655+ItsMeSousuke@users.noreply.github.com>
2021-08-17 22:43:46 +0200

    add position support for groups and support disabled item

---
+++ src/GumihoUI/Gumiho.Components/Options/Position.cs
@@ -0,0 +1,9 @@
+﻿namespace GumihoUI
+{
+    public static class Position
+    {
+        public static readonly string Left = "position-left";
+        public static readonly string Center = "position-center";
+        public static readonly string Right = "position-right";
+    }
+}
--- src/GumihoUI/Gumiho.Components/wwwroot/gumiho.js
+++ src/GumihoUI/Gumiho.Components/wwwroot/gumiho.js
@@ -1,6 +1,5 @@
 ﻿window.scrollToElement = (elementId) => {
     let element = document.getElementById(elementId)
-    console.log('aa', element)
 
     if (element) {
         element.scrollIntoView({ behavior: 'smooth' })
